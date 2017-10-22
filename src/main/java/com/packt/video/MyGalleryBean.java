0be1c54787958c8c08d9f76d3e4ba3ffa5255d4c
kit.perez <kit.perez@gmail.com>
2017-10-22 20:10:56 +1100

    - added MyPhotos and MyGalleryBean empty classes

---
+++ src/main/java/com/packt/video/MyGalleryBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.packt.video;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author kit.perez
+ */
+@Named(value = "myGalleryBean")
+@ViewScoped
+public class MyGalleryBean {
+
+    /**
+     * Creates a new instance of MyGalleryBean
+     */
+    public MyGalleryBean() {
+    }
+    
+}
---
+++ src/main/java/com/packt/video/MyPhoto.java
@@ -0,0 +1,15 @@
+package com.packt.video;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kit.perez
+ */
+public class MyPhoto {
+    
+}

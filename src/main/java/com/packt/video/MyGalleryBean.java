e75737b5e904af8e61a4fc73f0f7b776b1a59ea1
kit.perez <kit.perez@gmail.com>
2017-10-22 22:21:25 +1100

    populated galleryBean with list of photos from photos folder using NIO.2

--- src/main/java/com/packt/video/MyGalleryBean.java
+++ src/main/java/com/packt/video/MyGalleryBean.java
@@ -5,21 +5,60 @@
  */
 package com.packt.video;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import javax.servlet.ServletContext;
 
-/**
+/** This class is responsible to sustain upload process and provide collection 
+ * of photos that will be listed in the data table and the gallery.
  *
  * @author kit.perez
  */
 @Named(value = "myGalleryBean")
 @ViewScoped
-public class MyGalleryBean {
+public class MyGalleryBean implements Serializable {
+    
+    private List<MyPhoto> photos = new ArrayList<>();
 
     /**
      * Creates a new instance of MyGalleryBean
      */
     public MyGalleryBean() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        Path path = Paths.get(((ServletContext) externalContext.getContext())
+                .getRealPath(File.separator + "resources" + File.separator + "photos"));
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(path)) {
+            for (Path file: ds) {
+                MyPhoto photo = new MyPhoto(file.getFileName().toString(), false);
+                photos.add(photo);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(MyGalleryBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
+    public List<MyPhoto> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<MyPhoto> photos) {
+        this.photos = photos;
+    }
+    
+    
     
 }
